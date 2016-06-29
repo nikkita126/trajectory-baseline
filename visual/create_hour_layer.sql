148788a4a78a363c5f7bbcb065f31378ba62fa70
nikkita126 <catespinozar+github@gmail.com>
2016-06-29 10:11:59 -0400

    removed visual folder

--- visual/create_hour_layer.sql
+++
@@ -1,4 +0,0 @@
-﻿SELECT p.gid, p.geom, a.st_time,a.end_time,a.time_sum*5 as time_sum,a.traj_count,a.min_time*5 as min_time, a.max_time*5 as max_time, ((a.time_sum*5)::float/a.traj_count::float)::float as promedio, s.zona777
-FROM parada_util as p, accesibilidad as a, stop_id_frec as s
-WHERE a.destiny = s.frec_id AND s.code = p.codigo
-    AND st_time=384 AND a.destiny != a.origin
--- visual/mean_calc.sql
+++
@@ -1,9 +0,0 @@
-﻿WITH stops7 as (
-SELECT p.gid, p.geom, a.st_time,a.end_time,a.time_sum*5,a.traj_count,a.min_time*5, a.max_time*5, (a.time_sum*5/a.traj_count)::float as promedio, s.zona777
-FROM parada_util as p, accesibilidad as a, stop_id_frec as s
-WHERE a.destiny = s.frec_id AND s.code = p.codigo
-    AND st_time=372 AND a.destiny != a.origin
-)
-SELECT stops7.zona777,AVG(promedio)::float
-FROM stops7
-GROUP BY stops7.zona777
